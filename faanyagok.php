19e71b4259fc3ab3d0c1f2b3f3fd58940a40682e
KatonaKoppany <76950463+KatonaKoppany@users.noreply.github.com>
2021-11-15 11:13:09 +0100

    Container class CSS formázás

--- faanyagok.php
+++ faanyagok.php
@@ -2,24 +2,24 @@
     <div class="container">
         <div class="img-container">
             <div class="card">
-                <img class="img-rounded" src="bukk.jpg" alt="bukk">
+                <img class="img-rounded" src="./img/bukk.jpg" alt="bukk">
                 <label class="label-style" for="bukk">Bükk</label>
             </div>
             <div class="card">
-                <img class="img-rounded" src="bukk.jpg" alt="bukk">
-                <label class="label-style" for="bukk">Bükk</label>
+                <img class="img-rounded" src="./img/cseresznye.jpg" alt="bukk">
+                <label class="label-style" for="bukk">cseresznye</label>
             </div>
             <div class="card">
-                <img class="img-rounded" src="bukk.jpg" alt="bukk">
-                <label class="label-style" for="bukk">Bükk</label>
+                <img class="img-rounded" src="./img/dio.jpg" alt="bukk">
+                <label class="label-style" for="bukk">Dio</label>
             </div>
             <div class="card">
-                <img class="img-rounded" src="bukk.jpg" alt="bukk">
-                <label class="label-style" for="bukk">Bükk</label>
+                <img class="img-rounded" src="./img/mahagoni.jpg" alt="bukk">
+                <label class="label-style" for="bukk">Mahagoni</label>
             </div>
             <div class="card">
-                <img class="img-rounded" src="bukk.jpg" alt="bukk">
-                <label class="label-style" for="bukk">Bükk</label>
+                <img class="img-rounded" src="./img/tolgy.jpg" alt="bukk">
+                <label class="label-style" for="bukk">Tölgy</label>
             </div>
         </div>
     </div>
