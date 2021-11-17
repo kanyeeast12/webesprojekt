58d8eaa572b1c575e44d0c9480d7fe323afae35e
KatonaKoppany <76950463+KatonaKoppany@users.noreply.github.com>
2021-11-17 12:16:39 +0100

    Home page CSS formázás

--- rolunk.php
+++ rolunk.php
@@ -4,21 +4,21 @@
             <p>
                 19-20 évesek vagyunk, a Türr ben tanulunk, órai munkának eme csodás weboldal elkészítését kaptuk. <br> Neve: Kókány weblap 2.0
             </p>
-
+        <h1>Koppány </h1>
         <div class="about-container">
-            <h1>Koppány </h1>
+            
             <img src="./img/kopi.jpg" alt="" srcset="" width="100px" height="100px"> <!--Bal oldalra kell rakni -->
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ipsum tempora voluptatum facere consectetur dolores beatae hic enim, aut, sunt eaque consequatur officia labore iure qui! Quod ipsa sequi praesentium.</p>
         </div>
 
+        <h1>Adrián</h1>
         <div class="about-container">
-            <h1>Adrián</h1>
             <img src="./img/adrian.jpg" alt="" srcset="" width="100px" height="100px"> <!--Jobb oldalra kell rakni -->
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel omnis vero et tempora quisquam quidem dicta eum quibusdam maxime necessitatibus, soluta aperiam blanditiis laborum quis dolorum, nemo, reiciendis ad hic.</p>
         </div>
-
+        
+        <h1>Ricsko</h1>
         <div class="about-container">
-            <h1>Ricsko</h1>
             <img src="./img//ricsko.jpg" alt="" srcset="" width="100px" height="100px"> <!--Bal oldalra kell rakni -->
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo sunt, numquam sit aliquam, accusamus omnis mollitia dolores fuga id suscipit ad architecto corrupti! Sequi mollitia neque ea a beatae in.</p>
         </div>
