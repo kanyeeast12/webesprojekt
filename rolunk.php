8885f18c7234d711e1dd0cedb0ed699fb5632fd8
KatonaKoppany <76950463+KatonaKoppany@users.noreply.github.com>
2021-11-17 11:27:26 +0100

    About-container CSS formázás

--- rolunk.php
+++ rolunk.php
@@ -2,21 +2,24 @@
     <div class="container">
             <h1>Rólunk</h1>
             <p>
-               19-20 évesek vagyunk, a Türr ben tanulunk, órai munkának eme csodás weboldal elkészítését kaptuk. <br> Neve: Kókány weblap 2.0
+                19-20 évesek vagyunk, a Türr ben tanulunk, órai munkának eme csodás weboldal elkészítését kaptuk. <br> Neve: Kókány weblap 2.0
             </p>
 
-        <div id="kopi">
-            <h1>Koppány <br><img src="./img/kopi.jpg" alt="" srcset="" width="100px" height="100px"> </h1> <!--Bal oldalra kell rakni -->
+        <div class="about-container">
+            <h1>Koppány </h1>
+            <img src="./img/kopi.jpg" alt="" srcset="" width="100px" height="100px"> <!--Bal oldalra kell rakni -->
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ipsum tempora voluptatum facere consectetur dolores beatae hic enim, aut, sunt eaque consequatur officia labore iure qui! Quod ipsa sequi praesentium.</p>
         </div>
 
-        <div id="adrian">
-             <h1>Adrián <br> <img src="./img/adrian.jpg" alt="" srcset="" width="100px" height="100px"> </h1> <!--Jobb oldalra kell rakni -->
+        <div class="about-container">
+            <h1>Adrián</h1>
+            <img src="./img/adrian.jpg" alt="" srcset="" width="100px" height="100px"> <!--Jobb oldalra kell rakni -->
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel omnis vero et tempora quisquam quidem dicta eum quibusdam maxime necessitatibus, soluta aperiam blanditiis laborum quis dolorum, nemo, reiciendis ad hic.</p>
         </div>
 
-        <div id="ricsko">
-            <h1>Ricsko <br> <img src="./img//ricsko.jpg" alt="" srcset="" width="100px" height="100px"></h1> <!--Bal oldalra kell rakni -->
+        <div class="about-container">
+            <h1>Ricsko</h1>
+            <img src="./img//ricsko.jpg" alt="" srcset="" width="100px" height="100px"> <!--Bal oldalra kell rakni -->
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo sunt, numquam sit aliquam, accusamus omnis mollitia dolores fuga id suscipit ad architecto corrupti! Sequi mollitia neque ea a beatae in.</p>
         </div>
     </div>
