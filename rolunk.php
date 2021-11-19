be6b50f3bdd3abad8072e622ba48fd5b0ea72f37
Ricsko <szudiricsi@gmail.com>
2021-11-19 08:32:59 +0100

    container lekerekítése, rólunk php h1 es címek h2-essé alakítása

--- rolunk.php
+++ rolunk.php
@@ -6,20 +6,20 @@
                     19-20 évesek vagyunk, a Türr ben tanulunk, órai munkának eme csodás weboldal elkészítését kaptuk. <br> Neve: Kókány weblap 2.0
                 </b>
             </p>
-        <h1>Koppány </h1>
+        <h2>Koppány </h2>
         <div class="about-container">
             
             <img src="./img/kopi.jpg" alt="" srcset="" width="100px" height="100px"> <!--Bal-->
             <p><b>A csapat agya, mindent is tud. Leginkább a css-hez ért.</b></p>
         </div>
 
-        <h1>Adrián</h1>
+        <h2>Adrián</h2>
         <div class="about-container">
             <img src="./img/adrian.jpg" alt="" srcset="" width="100px" height="100px"> <!--Jobb-->
             <p><b>A csapat bolondja, szereti lógatni a lábát, és semmittenni. Leginkább a ctrl-c ctrl-v hez ért.</b></p>
         </div>
         
-        <h1>Ricsko</h1>
+        <h2>Ricsko</h2>
         <div class="about-container">
             <img src="./img//ricsko.jpg" alt="" srcset="" width="100px" height="100px"> <!--Bal-->
             <p><b>Adrián és Koppány között áll. Szeret semmittenni, de ha dolgozni kell dolgozik is.</b></p>
