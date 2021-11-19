f3f832a6774df351dfaa7be6ee92518ea16ef741
Ricsko <szudiricsi@gmail.com>
2021-11-19 08:25:39 +0100

    rolunk php szöveggel feltöltve fixed #4

--- rolunk.php
+++ rolunk.php
@@ -9,19 +9,19 @@
         <h1>Koppány </h1>
         <div class="about-container">
             
-            <img src="./img/kopi.jpg" alt="" srcset="" width="100px" height="100px"> <!--Bal oldalra kell rakni -->
+            <img src="./img/kopi.jpg" alt="" srcset="" width="100px" height="100px"> <!--Bal-->
             <p><b>A csapat agya, mindent is tud. Leginkább a css-hez ért.</b></p>
         </div>
 
         <h1>Adrián</h1>
         <div class="about-container">
-            <img src="./img/adrian.jpg" alt="" srcset="" width="100px" height="100px"> <!--Jobb oldalra kell rakni -->
+            <img src="./img/adrian.jpg" alt="" srcset="" width="100px" height="100px"> <!--Jobb-->
             <p><b>A csapat bolondja, szereti lógatni a lábát, és semmittenni. Leginkább a ctrl-c ctrl-v hez ért.</b></p>
         </div>
         
         <h1>Ricsko</h1>
         <div class="about-container">
-            <img src="./img//ricsko.jpg" alt="" srcset="" width="100px" height="100px"> <!--Bal oldalra kell rakni -->
+            <img src="./img//ricsko.jpg" alt="" srcset="" width="100px" height="100px"> <!--Bal-->
             <p><b>Adrián és Koppány között áll. Szeret semmittenni, de ha dolgozni kell dolgozik is.</b></p>
         </div>
     </div>
