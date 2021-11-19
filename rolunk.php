eef93ea69a407393e594ccbe822a5f409f15836f
Ricsko <szudiricsi@gmail.com>
2021-11-19 08:23:46 +0100

    szerszamkatalogus & blog php létrehozása fixed # 11 fixed #10

--- rolunk.php
+++ rolunk.php
@@ -2,25 +2,27 @@
     <div class="container">
             <h1>Rólunk</h1>
             <p>
-                19-20 évesek vagyunk, a Türr ben tanulunk, órai munkának eme csodás weboldal elkészítését kaptuk. <br> Neve: Kókány weblap 2.0
+                <b>
+                    19-20 évesek vagyunk, a Türr ben tanulunk, órai munkának eme csodás weboldal elkészítését kaptuk. <br> Neve: Kókány weblap 2.0
+                </b>
             </p>
         <h1>Koppány </h1>
         <div class="about-container">
             
             <img src="./img/kopi.jpg" alt="" srcset="" width="100px" height="100px"> <!--Bal oldalra kell rakni -->
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ipsum tempora voluptatum facere consectetur dolores beatae hic enim, aut, sunt eaque consequatur officia labore iure qui! Quod ipsa sequi praesentium.</p>
+            <p><b>A csapat agya, mindent is tud. Leginkább a css-hez ért.</b></p>
         </div>
 
         <h1>Adrián</h1>
         <div class="about-container">
             <img src="./img/adrian.jpg" alt="" srcset="" width="100px" height="100px"> <!--Jobb oldalra kell rakni -->
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel omnis vero et tempora quisquam quidem dicta eum quibusdam maxime necessitatibus, soluta aperiam blanditiis laborum quis dolorum, nemo, reiciendis ad hic.</p>
+            <p><b>A csapat bolondja, szereti lógatni a lábát, és semmittenni. Leginkább a ctrl-c ctrl-v hez ért.</b></p>
         </div>
         
         <h1>Ricsko</h1>
         <div class="about-container">
             <img src="./img//ricsko.jpg" alt="" srcset="" width="100px" height="100px"> <!--Bal oldalra kell rakni -->
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo sunt, numquam sit aliquam, accusamus omnis mollitia dolores fuga id suscipit ad architecto corrupti! Sequi mollitia neque ea a beatae in.</p>
+            <p><b>Adrián és Koppány között áll. Szeret semmittenni, de ha dolgozni kell dolgozik is.</b></p>
         </div>
     </div>
 <?php include 'footer.php' ?>
