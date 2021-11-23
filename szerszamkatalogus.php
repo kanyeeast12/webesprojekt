8aff13945b6494ef69305f0defce5cb75bf7a194
Ricsko <szudiricsi@gmail.com>
2021-11-23 08:58:02 +0100

    képek hozzáadása, szerszámkatalógus.php kezdetleges felépítése

--- szerszamkatalogus.php
+++ szerszamkatalogus.php
@@ -1,4 +1,27 @@
 <?php include 'header.php' ?>
+    <div class="container">
+    <h1>Válasszon szerszámot!</h1>
+    <br>
+        <div class="szerszam-container">
+            <div class="szerszam-card">
+                <h2>Szikramentes szerszámok</h2>
+                <p><b>Gyártó: </b>Sanyi</p>
+                <p><b>Felhasználás: </b>Akárhol</p>
+                <p><b>Mennyiség </b>Képen látható</p>
+                <p><b>Egységár </b>1 500 Ft/db</p>
+                <img src="./img/szikramentes.jpg" alt="szikramentes" width="200px">
+            </div>
+        </div>
 
-
+        <div class="szerszam-container">
+            <div class="szerszam-card">
+                <h2>Építéshez való szerszámok</h2>
+                <p><b>Gyártó: </b>Béla</p>
+                <p><b>Felhasználás: </b>Akárhol</p>
+                <p><b>Mennyiség </b>Képen látható</p>
+                <p><b>Egységár </b>1 500 Ft/db</p>
+                <img src="./img/epiteshez.jpg" alt="vodor" width="200px">
+            </div>
+        </div>
+    </div>
 <?php include 'footer.php' ?>
