6f073868e3929c976094ed2f377446376285289b
KatonaKoppany <76950463+KatonaKoppany@users.noreply.github.com>
2021-11-23 19:29:07 +0100

    Egyszerű CSS formázás(Szerszámok)

--- szerszamkatalogus.php
+++ szerszamkatalogus.php
@@ -11,9 +11,46 @@
                 <p><b>Egységár </b>1 500 Ft/db</p>
                 <img src="./img/szikramentes.jpg" alt="szikramentes" width="200px">
             </div>
-        </div>
-
-        <div class="szerszam-container">
+            <div class="szerszam-card">
+                <h2>Építéshez való szerszámok</h2>
+                <p><b>Gyártó: </b>Béla</p>
+                <p><b>Felhasználás: </b>Akárhol</p>
+                <p><b>Mennyiség </b>Képen látható</p>
+                <p><b>Egységár </b>1 500 Ft/db</p>
+                <img src="./img/epiteshez.jpg" alt="vodor" width="200px">
+            </div>
+            <div class="szerszam-card">
+                <h2>Építéshez való szerszámok</h2>
+                <p><b>Gyártó: </b>Béla</p>
+                <p><b>Felhasználás: </b>Akárhol</p>
+                <p><b>Mennyiség </b>Képen látható</p>
+                <p><b>Egységár </b>1 500 Ft/db</p>
+                <img src="./img/epiteshez.jpg" alt="vodor" width="200px">
+            </div>
+            <div class="szerszam-card">
+                <h2>Építéshez való szerszámok</h2>
+                <p><b>Gyártó: </b>Béla</p>
+                <p><b>Felhasználás: </b>Akárhol</p>
+                <p><b>Mennyiség </b>Képen látható</p>
+                <p><b>Egységár </b>1 500 Ft/db</p>
+                <img src="./img/epiteshez.jpg" alt="vodor" width="200px">
+            </div>
+            <div class="szerszam-card">
+                <h2>Építéshez való szerszámok</h2>
+                <p><b>Gyártó: </b>Béla</p>
+                <p><b>Felhasználás: </b>Akárhol</p>
+                <p><b>Mennyiség </b>Képen látható</p>
+                <p><b>Egységár </b>1 500 Ft/db</p>
+                <img src="./img/epiteshez.jpg" alt="vodor" width="200px">
+            </div>
+            <div class="szerszam-card">
+                <h2>Építéshez való szerszámok</h2>
+                <p><b>Gyártó: </b>Béla</p>
+                <p><b>Felhasználás: </b>Akárhol</p>
+                <p><b>Mennyiség </b>Képen látható</p>
+                <p><b>Egységár </b>1 500 Ft/db</p>
+                <img src="./img/epiteshez.jpg" alt="vodor" width="200px">
+            </div>
             <div class="szerszam-card">
                 <h2>Építéshez való szerszámok</h2>
                 <p><b>Gyártó: </b>Béla</p>
