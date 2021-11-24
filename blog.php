9af01e9c44daf977cc77109a6741dc7ddd68865b
playboicarti2009 <lazaradrian0718@gmail.com>
2021-11-24 12:24:27 +0100

    blog

--- blog.php
+++ blog.php
@@ -1,4 +1,34 @@
 <?php include 'header.php' ?>
+<div class="hatter">
+<div class="blogh">
+<h1>FOLYÓKÁK A HÁZ KÖRÜL</h1>
+</div>
 
+<div class="blogp">
+<p>Habár a télnek az elmúlt napokban búcsút inthettünk, a hidegebb időjárás azonban még beköszönthet a tavasz folyamán is, nem is beszélve a nagyobb esőkről, melyek az előző napokban, hetekben messze elkerültek minket. Esőzés a nyár beköszöntéig gyakrabban várható, így az udvari folyókák szerepe a következő hónapokban is felértékelődik. A folyókák akár a csúszásveszély elhárításában is komoly szerepet játszhatnak, igaz ritka, hogy a tavaszi talajmenti fagyok esővel járjanak együtt, de a következő télen, a hóolvadással járó visszafagyás veszélye sem elhanyagolható tényező, ha folyókák telepítéséről van szó. De pontosan melyek is ezek a folyókák?</p>
+</div>
+
+<div class="ibbigang">
+<img src="img/kecske.jpg" alt="waa" width="315" height="260">
+</div>
+
+<div class="blogp">
+    <p>
+    Ha a frissen (és szakszerűen) térkövezett felületeket veszünk górcső alá, biztos felkelti a figyelmünket fel- felbukkanó, a burkolat szintjében álló hosszúkás rácsok sokasága. Elsősorban nagyobb, egybefüggő felületek esetén találkozhatunk velük, vagy olyan helyeken ahol kicsi a csapadék gyors felszívódásának esélye, vagy netán a környező tereptárgyakat kell megóvni a komolyabb nedvességmennyiségtől.
+    </p>
+    </div>
+    <div class="ibbigang2">
+<img src="img/hetebreturo.jpg" alt="waa" width="315" height="260">
+</div>
+<div class="blogp">
+<p>
+Sokan az autóbejárók vízelvezetésével azonosítják, mivel a legtöbb esetben itt láthatunk nagyobb, egybefüggő, korlátozott lefolyású felületeket. Feltűnése azonban nem az autóforgalommal, hanem a közlekedésbiztonsággal, és az épített környezet védelmével hozható összefüggésbe. Így akár már gyalogjárda esetén is szükségünk lehet rá, a kérdés csak az, hogy mely típus lesz a számunkra legmegfelelőbb.
+Ha összehasonlítjuk a hagyományos kültéri folyókákat, gyorsan feltűnhet, hogy ezek leggyakrabban a térkőgyártók kínálatával szorosan összefűzve jelennek meg, még ha gyártójuk olykor-olykor nem is azonos a térköveket gyártókéval. Ma legnagyobbrészt elemes beton térköveket használunk térburkolásra, hiszen gyorsan fektethető, könnyen tisztítható, jó teherbíró képességű emellett és ápolásmentes felületet biztosít. Hogy a folyóka is gond nélkül teljesítse ezeket a feltételeket, modern gyártástechnológiára van szükség.
+</p>
+</div>
+<div class="ibbigang">
+<img src="img/grillsuto.jpg" alt="waa" width="315" height="260">
+</div>
+</div>
 
 <?php include 'footer.php' ?>
