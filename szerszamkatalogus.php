6975cc7fe3081c96ce483511b9a1ab70586eab99
Ricsko <szudiricsi@gmail.com>
2021-11-29 09:58:22 +0100

    szerszamkatalogus phpn magassag adasa a kepenek

--- szerszamkatalogus.php
+++ szerszamkatalogus.php
@@ -5,27 +5,27 @@
         <div class="szerszam-container">
             <div class="szerszam-card">
                 <h2>Szikramentes szerszámok</h2>
-                <img src="./img/szikramentes.jpg" alt="szikramentes" width="200px">
+                <img src="./img/szikramentes.jpg" alt="szikramentes" width="200px" height="200px">
             </div>
             <div class="szerszam-card">
                 <h2>Építéshez való szerszámok</h2>
-                <img src="./img/epiteshez.jpg" alt="epiteshez" width="200px">
+                <img src="./img/epiteshez.jpg" alt="epiteshez" width="200px" height="200px">
             </div>
             <div class="szerszam-card">
                 <h2>Colostok</h2>
-                <img src="./img/colostok.jpg" alt="colostok" width="200px">
+                <img src="./img/colostok.jpg" alt="colostok" width="200px" height="200px">
             </div>
             <div class="szerszam-card">
                 <h2>Csavarhúzók</h2>
-                <img src="./img/csavarhuzok.jpg" alt="csavarhuzok" width="200px">
+                <img src="./img/csavarhuzok.jpg" alt="csavarhuzok" width="200px" height="200px">
             </div>
             <div class="szerszam-card">
                 <h2>Pajszer</h2>
-                <img src="./img/pajszer.jpg" alt="pajszer" width="200px">
+                <img src="./img/pajszer.jpg" alt="pajszer" width="200px" height="200px">
             </div>
             <div class="szerszam-card">
                 <h2> Flex </h2>
-                <img src="./img/flex.jpg" alt="flex" width="200px">
+                <img src="./img/flex.jpg" alt="flex" width="200px" height="200px">
             </div>
         </div>
     </div>
